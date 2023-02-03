5821c8ba6baebc13631f714cd79470dc63783c05
Luis Moral Guerrero <luis.moral@gmail.com>
2023-02-03 20:01:23 +0100

    Added base spring boot files.

---
+++ src/test/java/backdev/ApplicationShould.java
@@ -0,0 +1,13 @@
+package backdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationShould {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
