6e9020b333d253bd2376d7d39ef7d366c326cb94
Luis Moral Guerrero <luis.moral@gmail.com>
2023-02-04 11:44:00 +0100

    Added similar products feature test.

---
+++ src/main/java/backdev/handler/status/get/GetStatusHandler.java
@@ -0,0 +1,12 @@
+package backdev.handler.status.get;
+
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+public class GetStatusHandler {
+
+    public Mono<ServerResponse> status(ServerRequest request) {
+        return ServerResponse.ok().body(Mono.just("OK"), String.class);
+    }
+}
---
+++ src/test/java/backdev/SimilarProductsFeature.java
@@ -0,0 +1,50 @@
+package backdev;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.List;
+
+@ActiveProfiles(profiles = "test")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = Application.class)
+public class SimilarProductsFeature {
+
+    @Value("${endpoint.product.path.similar}")
+    private String similarProductsEndpoint;
+
+    @Autowired
+    private WebTestClient webClient;
+
+    @Test public void
+    return_similar_products() {
+        webClient
+            .get()
+                .uri(builder -> builder.path(similarProductsEndpoint).build("1"))
+            .exchange()
+                .expectStatus()
+                    .isOk()
+                .expectBody(new ParameterizedTypeReference<List<ResponseItem>>() {})
+                    .consumeWith(response ->
+                        {
+                            List<ResponseItem> items = response.getResponseBody();
+
+                            Assertions
+                                .assertThat(items)
+                                .containsExactlyInAnyOrder(
+                                    new ResponseItem("2", "product2", 10.0, true),
+                                    new ResponseItem("3", "product3", 15.0, false),
+                                    new ResponseItem("4", "product4", 17.50, true)
+                                );
+                        }
+                    );
+    }
+
+    private record ResponseItem(String id, String name, double number, boolean availability) {
+    }
+}
